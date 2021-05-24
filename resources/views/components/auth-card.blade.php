6b217a169491a9ba50c07570f36a882db4ca674f
natchez <natchezzz@hotmail.com>
2021-05-24 22:45:20 +0200

    auth fini,a tester et modifier

---
+++ resources/views/components/auth-card.blade.php
@@ -0,0 +1,6 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div class="w-full min-h-screen flex items-stretch text-white shadow-md overflow-hidden ">
+        {{ $slot }}
+    </div>
+
+</div>
